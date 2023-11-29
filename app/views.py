84800481c78a50960e2e643c92ef9829fc0039fc
SravaniCS <sravanisravya772@gmail.com>
2023-11-29 21:09:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def carousel_01(request):
+    return render(request,'carousel_01.html')
+
+def carousel_02(request):
+    return render(request,'carousel_02.html')
